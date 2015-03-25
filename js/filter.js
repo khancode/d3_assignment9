3be8ace51cf21d0fe3f6e52074cf72dd505809b5
Omar Khan <khancodegt@gmail.com>
2015-03-25 00:37:10 -0400

    added bar_chart.js viz with data.tsv

---
+++ js/filter.js
@@ -0,0 +1,46 @@
+/**
+ * Created by khancode on 3/24/2015.
+ */
+
+//$.getScript("js/index.js", function(){
+//
+//    console.log("Script loaded and executed.");
+//    // here you can use anything you defined in the loaded script
+//
+//    setSelector();
+//
+//});
+
+$filter = new Filter();
+
+function Filter() {
+    this.setSelector = function() {
+
+        $(".legend").hover(
+            function(){
+                var $this = $(this).find("rect");
+                $this.data('fill', $this.css('fill')).css('fill', '#F0F0F0');
+            },
+            function(){
+                var $this = $(this).find("rect");
+                $this.css('fill', $this.data('fill'));
+            }
+        );
+
+        $(".legend").click(function () {
+            //alert('it worked');
+
+            var category = $(this).find("text").text();
+            print(category);
+
+            $stacked_bar_chart.removeFarm();
+            $bar_chart.farm();
+        });
+    }
+};
+
+
+/* Convenience of printing to console */
+this.print = function(str) {
+    console.log(str);
+};
